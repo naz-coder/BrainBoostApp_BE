52009e129901daa96c0bf19f38417fe27b66b059
naz-coder <favouritenaz@yahoo.com>
2024-09-11 11:58:06 +0100

    User progress enpoint updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 const User = require("../model/user");
 
 exports.updateUserProgress = async(req, res) => {
-    const {userId, updateData} = re.body;
+    const {id} = req.params;
+    const {updateData} = req.body;
 
     try{
-        await User.findByIdAndUpdate(userId, {Spush: updateData});
+        await User.findByIdAndUpdate(id, {Spush: updateData});
         res.status(200).json({message: "User progress updated successfully"});
     }catch(error){
         res.status(500).json({error: "Error updating user progress", details: error});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.put("/update-progress", userController.updateUserProgress);
+router.put("/:id/progress", userController.updateUserProgress);
 
 module.exports = router;
