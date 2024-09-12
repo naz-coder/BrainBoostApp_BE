a8c93b0b8bb079f59de447331c1dc80a694eae18
naz-coder <favouritenaz@yahoo.com>
2024-09-12 18:23:26 +0100

    Logout endpoint fixed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const verifyToken = require("../middleware/authMiddleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/logout", authController.logout);
+router.post("/logout", verifyToken, authController.logout);
 
 module.exports = router;
